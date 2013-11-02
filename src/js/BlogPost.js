b2a10cf2de374dfc1caccba6c65b7181bd561ab4
Lars Thorup <lars@zealake.com>
2013-11-02 10:33:34 -0700

    leak detection

--- src/js/BlogPost.js
+++ src/js/BlogPost.js
@@ -3,6 +3,8 @@
     window.BlogPost = function (options) {
         this.options = options;
         this.createdOn = new Date();
+        // Note: simulate a DOM element leak
+        // $('<div id="overlay"></div>').appendTo('body');
     };
     window.BlogPost.prototype.age = function () {
         var now = new Date();
--- src/js/JasmineConfigure.js
+++ src/js/JasmineConfigure.js
@@ -7,6 +7,25 @@
         var fixtures = jasmine.getFixtures();
         fixtures.set(fixtures.sandbox());
 
+        // Element leak detection
+        spec.after(function () {
+            var leaks = $('body').children(':not(#HTMLReporter)');
+            expect(leaks).not.toExist();
+            leaks.remove();
+        });
+
+        // Heap logging
+        // Note: only works with Chrome started with --enable-memory-info --js-flags="--expose-gc"
+        if (window.performance) {
+            if (window.gc) { window.gc(); }
+            spec.heapSizeBefore = window.performance.memory.usedJSHeapSize;
+            spec.after(function () {
+                if(window.gc) { window.gc(); }
+                spec.heapSizeAfter = window.performance.memory.usedJSHeapSize;
+                console.log(spec.heapSizeAfter - spec.heapSizeBefore, spec.getFullName());
+            });
+        }
+
         // Mock time
         jasmine.Clock.useMock();
         jasmine.Clock.reset();
